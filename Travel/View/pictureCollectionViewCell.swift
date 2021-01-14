35acc285cfdb0bc2a075976d701160f56aef8c5a
ly <410721207@gms.ndhu.edu.tw>
2021-01-14 11:50:06 +0800

    haha

---
+++ Travel/View/pictureCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  pictureCollectionViewCell.swift
+//  Travel
+//
+//  Created by NDHU_CSIE on 2021/1/14.
+//  Copyright © 2021 NDHU_CSIE. All rights reserved.
+//
+
+import UIKit
+
+class pictureCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+}
